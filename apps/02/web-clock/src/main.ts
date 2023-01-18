baf7518e6be1f6610c63d591b8148129e18ad347
Rares Golea <rgolea@gmail.com>
2023-01-18 23:24:56 +0200

    Clock completion

--- apps/02/web-clock/src/main.ts
+++ apps/02/web-clock/src/main.ts
@@ -3,26 +3,40 @@ const minuteHand = document.querySelector<HTMLDivElement>('.min-hand');
 const secondHand = document.querySelector<HTMLDivElement>('.second-hand');
 const timeRectangle = document.querySelector<HTMLDivElement>('.time');
 
+const formatter = Intl.NumberFormat(navigator.language, {
+  minimumIntegerDigits: 2
+})
+
+const TIME_SPLITS = {
+  HOUR: 12,
+  MINUTES: 60,
+  SECONDS: 60,
+} as const;
+
+type TimePeriod = keyof typeof TIME_SPLITS;
+
+function calculateDegrees(time: number, period: TimePeriod){
+  return time / TIME_SPLITS[period] * 360 + 90;
+}
+
+function rotateDegrees(el: HTMLDivElement | null, degrees: number) {
+  if(!el) return;
+  el.style.transform = `rotate(${degrees}deg)`;
+}
+
 function setDate() {
-    const date = new Date();
-    const hour = date.getHours();
-    const minute = date.getMinutes();
-    const second = date.getSeconds();
+  const date = new Date();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
 
-    const secondDegree = ((second/60)*360) + 90;
-    const minuteDegree = ((minute/60)*360) + ((second/60)*6) + 90;
-    const hourDegree = ((hour/12)*360)+((minute/60)*30) + 90;
-    if(!hourHand || !minuteHand || !secondHand || !timeRectangle) return;
-    hourHand.style.transform = 'rotate('+hourDegree+'deg)';
-    minuteHand.style.transform = 'rotate('+minuteDegree+'deg)';
-    secondHand.style.transform = 'rotate('+secondDegree+'deg)';
-
-    timeRectangle.innerHTML = '<span>'+hour+': '+minute+': '+second+'</span>'
-
+  rotateDegrees(secondHand, calculateDegrees(second, 'SECONDS'));
+  rotateDegrees(minuteHand, calculateDegrees(minute, 'MINUTES') + second / 10);
+  rotateDegrees(hourHand, calculateDegrees(hour, 'HOUR') + (minute / 2));
+  if(timeRectangle) timeRectangle.innerHTML = `<span>${[hour, minute, second].map(formatter.format).join(':')}</span>`;
 }
 
 setInterval(setDate, 1000);
 setDate();
-
 
 export {};
